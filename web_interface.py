43b641483ea312cd32e86981683b06b86820ed6a
Petr Baudis <pasky@ucw.cz>
2015-09-21 17:46:54 +0200

    Log each asked question

--- web_interface.py
+++ web_interface.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from flask import Flask, render_template, request
 
 from main_frame import get_answer,get_sources,get_query
@@ -15,6 +17,7 @@ def generate_answer():
     question=request.form['question']
     answer = get_answer(question)
     (headline,url,body)=get_sources()
+    print('<<%s>> -> %s :: [%s :: %s]' % (question, answer, headline, url))
     return render_template('form_action.html', content='block', question=question, answer=answer,
                            headline=headline,url=url,body=body,query=get_query())
 
