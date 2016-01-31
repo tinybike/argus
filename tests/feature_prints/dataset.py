43b9dc9784ac2e0c3d76e20510a72f635cb2b706
Silvicek <Silvicek@gmail.com>
2016-01-31 17:35:02 +0100

    dataset split, \\n fix

--- tests/feature_prints/dataset.py
+++ tests/feature_prints/dataset.py
@@ -1,18 +1,27 @@
 """Extract question|sentence|gs class"""
 
 import csv
+import numpy as np
 
-table = []
-i = 0
+trainIDs = np.load('../trainIDs/trainIDs.npy')
+
+table_test = []
+table_train = []
 for line in csv.reader(open('all_features.tsv'), delimiter='\t'):
-    i+=1
-    if len(line)<3:
-        print line, i
     if line[2] not in '10':
         continue
-    table.append([line[0], line[2], line[1]])
+    if line[0] in trainIDs:
+        table_train.append([line[0], line[2], line[1].replace('\n', ' ')])
+    else:
+        table_test.append([line[0], line[2], line[1].replace('\n', ' ')])
 
-writer = csv.writer(open('argus_gen.csv', 'wb'), delimiter=',')
+
+writer = csv.writer(open('argus_train.csv', 'wb'), delimiter=',')
 writer.writerow(['qtext', 'label', 'atext'])
-for triplet in table:
+for triplet in table_train:
+    writer.writerow(triplet)
+
+writer = csv.writer(open('argus_test.csv', 'wb'), delimiter=',')
+writer.writerow(['qtext', 'label', 'atext'])
+for triplet in table_test:
     writer.writerow(triplet)
