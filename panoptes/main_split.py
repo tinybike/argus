f6b2643403b3bc542ab27658ec8e4f404d027d90
tealmill <trollmil@fel.cvut.cz>
2016-05-13 16:07:54 +0200

    Implemented the initial main script

---
+++ panoptes/main_split.py
@@ -0,0 +1,12 @@
+import sys
+import json
+from finanget import stockquery
+
+def proccess(que):
+    ans = stockquery(que)
+    if "Decision" in ans:
+        return ans
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as que:
+        print(proccess(que))
