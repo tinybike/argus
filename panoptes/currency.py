ce936b642a6e4b329cce4c379cf17890840d83f0
tealmill <trollmil@fel.cvut.cz>
2016-05-20 13:34:37 +0200

    working on currency

--- panoptes/currency.py
+++ panoptes/currency.py
@@ -3,8 +3,15 @@ import sys
 import json
 
 def makequery(question):
-    print(question)
 
+    if question["type"] != "currency":
+        print("Doesn't look like a currency query to me, can't do.")
+        answer = {"useful": False}
+        return answer
+
+    res = urllib.request.urlopen("http://api.fixer.io/"+question["datestart"])
+    response = json.loads(str(res))
+    print(response)
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as que:
